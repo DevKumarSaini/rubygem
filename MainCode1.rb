397a104208f49dccce3ea4c01f8c2b015cafa996
DevKumarSaini <devsaini703@gmail.com>
2023-04-26 17:16:05 +0530

    adding colour gem

--- MainCode1.rb
+++ MainCode1.rb
@@ -1,4 +1,5 @@
 require 'money'
+require 'colorize'
 require_relative 'replicaCurrency'
 
 
@@ -9,26 +10,26 @@ end
 
 
 
-puts "\nPlease Enter your Currency CODE  --\n"
-        puts "\n    COUNTRY         CURRENCY      CURRENCY-CODE\n\n"
-        puts "1)  Indian          (Rupees)          INR "
-        puts "2)  United State    (Dollar)          USD "
-        puts "3)  Europe          (Euro)            Eur"
-        puts "4)  Japnees         (Yen)             JPY"
-        puts "5)  Britain         (Pound)           GBP\n\n"
+puts "\nPlease Enter your Currency CODE  --\n".colorize(:blue)
+        puts "\n    COUNTRY         CURRENCY      CURRENCY-CODE\n\n".colorize(:red)
+        puts "1)  Indian          (Rupees)          INR ".colorize(:blue)
+        puts "2)  United State    (Dollar)          USD ".colorize(:blue)
+        puts "3)  Europe          (Euro)            Eur".colorize(:blue)
+        puts "4)  Japnees         (Yen)             JPY".colorize(:blue)
+        puts "5)  Britain         (Pound)           GBP\n\n".colorize(:blue)
         currencyCode = gets.chomp.upcase
 
-        puts "\nEnter Amoutn to CONVERT \n\n"
+        puts "\nEnter Amoutn to CONVERT \n\n".colorize(:red)
         amount = gets.chomp.to_i
 
         # current_curr = gets.chomp.upcase
-        puts "\nSelect target currency CODE to convert your Money\n"
-        puts "    COUNTRY         CURRENCY      CURRENCY-CODE\n"
-        puts "1)  Indian          (Rupees)          INR "
-        puts "2)  United State    (Dollar)          USD "
-        puts "3)  Europe          (Euro)            Eur"
-        puts "4)  Japnees         (Yen)             JPY"
-        puts "5)  Britain         (Pound)           GBP\n\n"
+        puts "\nSelect target currency CODE to convert your Money\n".colorize(:green)
+        puts "    COUNTRY         CURRENCY      CURRENCY-CODE\n".colorize(:green)
+        puts "1)  Indian          (Rupees)          INR ".colorize(:green)
+        puts "2)  United State    (Dollar)          USD ".colorize(:green)
+        puts "3)  Europe          (Euro)            Eur".colorize(:green)
+        puts "4)  Japnees         (Yen)             JPY".colorize(:green)
+        puts "5)  Britain         (Pound)           GBP\n\n".colorize(:green)
         changeCurrencyCode = gets.chomp.upcase
 
 
